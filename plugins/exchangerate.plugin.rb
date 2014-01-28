2723fbca872964f1934e5759168bb6309725ffb6
icanprogram <icanprogramfedoracoins@gmail.com>
2014-01-28 20:58:14 +0100

    Added crypsy, I need to clean this up

--- plugins/exchangerate.plugin.rb
+++ plugins/exchangerate.plugin.rb
@@ -5,45 +5,45 @@ require_relative '../include/coinslot.singleton.rb'
 
 class ExchangeRate
 	include Cinch::Plugin
-	match /rate(?:$| (ltc|btc)$)/, method: :last_trade
-	match /exchanges(?:$| (ltc|btc)$)/, method: :last_trade
-	match /ticker(?:$| (ltc|btc)$)/, method: :last_trade
-	match /price(?:$| (ltc|btc)$)/, method: :last_trade
+	match /rate(?:$| (ltc|doge)$)/, method: :last_trade
+	match /exchanges(?:$| (ltc|doge)$)/, method: :last_trade
+	match /ticker(?:$| (ltc|doge)$)/, method: :last_trade
+	match /price(?:$| (ltc|doge)$)/, method: :last_trade
 	
-	match /rate (?:|(ltc|btc) )([0-9]+)$/, method: :worth
-	match /exchanges (?:|(ltc|btc) )([0-9]+)$/, method: :worth
-	match /ticker (?:|(ltc|btc) )([0-9]+)$/, method: :worth
-	match /price (?:|(ltc|btc) )([0-9]+)$/, method: :worth
+	match /rate (?:|(ltc|doge) )([0-9]+)$/, method: :worth
+	match /exchanges (?:|(ltc|doge) )([0-9]+)$/, method: :worth
+	match /ticker (?:|(ltc|doge) )([0-9]+)$/, method: :worth
+	match /price (?:|(ltc|doge) )([0-9]+)$/, method: :worth
 	
 	set :help, <<-HELP
-!rate [ltc|btc|null] [amount]
+!rate [ltc|doge|null] [amount]
 	Shows the latest trade on coinedup, worth of amount if specified
-!exchanges [ltc|btc|null] [amount]
+!exchanges [ltc|doge|null] [amount]
 	Shows the latest trade on coinedup, worth of amount if specified
-!ticker [ltc|btc|null] [amount]
+!ticker [ltc|doge|null] [amount]
 	Shows the latest trade on coinedup, worth of amount if specified
-!price [ltc|btc|null] [amount]
+!price [ltc|doge|null] [amount]
 	Shows the latest trade on coinedup, worth of amount if specified
 	HELP
 	
 	def last_trade(m, type)
-		return if not CoinSlot.instance.check_coinslot(m)
-		type = "BTC" if type.nil?
+		#return if not CoinSlot.instance.check_coinslot(m)
+		type = "LTC" if type.nil?
 		type.upcase!
 		
-		trade = get_last_trade type
+		trades = get_last_trades type
 		
-		time = Time.at(trade['timestamp'].to_i).utc
-		
-		m.reply "Latest trade on CoinedUp: #{trade['amount']}@#{trade['price']}#{trade['curr']} on #{time}"
+		trades.each do |exchange, trade|
+			m.reply "Latest trade on #{exchange}: #{trade['amount']}@#{trade['price']}#{trade['curr']} on #{trade['time']}"
+		end
 	end
 	
 	def worth(m, type, amount)
-		return if not CoinSlot.instance.check_coinslot(m)
-		type = "BTC" if type.nil?
+		#return if not CoinSlot.instance.check_coinslot(m)
+		type = "LTC" if type.nil?
 		type.upcase!
 		
-		trade = get_last_trade type
+		trade = process_trade(coinedup_to_trade(get_coinedup(type)))
 		
 		total = (amount.to_i * trade['price']).to_f
 		
@@ -59,10 +59,7 @@ class ExchangeRate
 		m.reply "#{total}#{trade['curr']}"
 	end
 	
-	def get_last_trade type
-		response = CurbFu.get("http://192.168.1.100/coins/coinedup.php?type=#{type}")
-		trade = JSON.parse(response.body)
-		
+	def process_trade trade
 		if trade['curr'] == "BTC" or trade['curr'] == "LTC"
 			price = trade['price'].to_f
 			if price < 0.00001
@@ -74,4 +71,58 @@ class ExchangeRate
 		
 		trade
 	end
+	
+	def get_coinedup type
+		response = CurbFu.get("http://192.168.1.100/coins/coinedup.php?type=#{type}")
+		JSON.parse(response.body)
+	end
+	
+	def get_cryptsy type
+		if type == "LTC"
+			begin
+				response = CurbFu.get("http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=147")
+				data = JSON.parse(response.body)['return']['markets']['TIPS']
+				ret = data['recenttrades'][0]
+				ret['curr'] = data['secondarycode']
+				ret
+			rescue
+				ret = Hash.new
+				ret['price'] = 0
+				ret['quantity'] = 0
+				ret['time'] = ""
+				ret['secondarycode'] = "API is down"
+				ret
+			end
+		else
+			nil
+		end
+	end
+	
+	def coinedup_to_trade coinedup
+		coinedup['time'] = Time.at(coinedup['timestamp'].to_i).utc
+		coinedup
+	end
+	
+	def cryptsy_to_trade cryptsy
+		puts cryptsy.inspect
+		trade = Hash.new
+		trade['price'] = cryptsy['price']
+		trade['amount'] = cryptsy['quantity']
+		trade['time'] = cryptsy['time'] + " Cryptsy time"
+		trade['curr'] = cryptsy['curr']
+		trade
+	end
+	
+	def get_last_trades type
+		ret = Hash.new
+		if type == "LTC"
+			ret["CoinedUp"] = process_trade(coinedup_to_trade(get_coinedup(type)))
+			ret["Cryptsy"] = process_trade(cryptsy_to_trade(get_cryptsy(type)))
+		elsif type == "DOGE"
+			ret["CoinedUp"] = process_trade(coinedup_to_trade(get_coinedup(type)))
+		end
+		ret
+	end
+	
+	
 end
